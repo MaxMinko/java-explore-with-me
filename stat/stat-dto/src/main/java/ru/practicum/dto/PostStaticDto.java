8f0b3bb30c0a939f6305ec993adbcca772997b9b
Max Minko <minko.maks2015@yandex.ru>
2023-07-14 14:29:41 +0300

    "Add  main-service. "

--- stat/stat-dto/src/main/java/ru/practicum/dto/PostStaticDto.java
+++ stat/stat-dto/src/main/java/ru/practicum/dto/PostStaticDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-import java.net.http.HttpRequest;
+
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
